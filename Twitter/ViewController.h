bad01e5bdeadaa3fb511fcbad40f5464fe0e0038
Yuning Jin <yuningj@yahoo-inc.com>
2016-09-19 16:35:05 -0700

    Initial Commit

---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Yuning Jin on 9/19/16.
+//  Copyright © 2016 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Twitter
+//
+//  Created by Yuning Jin on 9/19/16.
+//  Copyright © 2016 Yahoo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
